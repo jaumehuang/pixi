d8d1d793716e54a2fde1fb6ad732107fe7bfbee7
jiahao <1332204564@qq.com>
2020-10-08 21:11:19 +0800

    初始化pixi

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-09-30 16:10:24
- * @LastEditTime: 2020-10-07 23:01:02
+ * @LastEditTime: 2020-10-08 17:52:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \pixi\src\page\index.vue
@@ -10,22 +10,19 @@
   <div class="loading" id="loading">加载中0%</div>
 </template>
 <script>
-import * as PIXI from 'pixi.js'
+import * as PIXI from "pixi.js";
 let Application = PIXI.Application,
-    Container = PIXI.Container,
-    loader = PIXI.loader,
-    resources = PIXI.loader.resources,
-    TextureCache = PIXI.utils.TextureCache,
-    Sprite = PIXI.Sprite;
+  Container = PIXI.Container,
+  loader = PIXI.loader,
+  resources = PIXI.loader.resources,
+  TextureCache = PIXI.utils.TextureCache,
+  Sprite = PIXI.Sprite;
 export default {
   data() {
     return {};
   },
   created() {
-   
-  },
-  mounted() {
-     this.initPixi();
+    this.initPixi();
   },
   methods: {
     /**
@@ -35,46 +32,51 @@ export default {
      */
     initPixi() {
       const w = document.body.clientWidth;
-      const  h = document.body.clientHeight; 
-    let app = new Application({ 
-        width:w,
-        height:800,                      
-        antialiasing: true, 
-        transparent: false, 
+      const h = document.body.clientHeight;
+      let app = new Application({
+        width: w,
+        height: 800,
+        antialiasing: true,
+        transparent: false,
         resolution: 1,
         backgroundColor: 0x1d9ce0,
+      });
+      document.body.appendChild(app.view);
+      // 获取屏幕宽高，判断横屏还是竖屏
+      let min = w < h ? w : h;
+      let scale = min / 750; // 根据设计稿尺寸进行缩放比例调整
+      app.stage.scale.set(scale, scale); // 根据屏幕实际宽高放大舞台
+      //导入资源
+      loader
+        .add("bed_boy", require("../imgs/bed_boy.png"))
+        .add("bed", require("../imgs/bed.png"))
+        .load(setup);
+      //进度加载
+      loader.onProgress.add((target) => {
+        document.getElementById("loading").innerText =
+          parseInt(target.progress) + "%";
+      });
+      //加载完成
+      loader.onComplete.add((target) => {
+        document.getElementById("loading").style.display = "none";
+      });
+      //渲染组件
+      function setup() {
+        let boy = new Sprite(resources["bed_boy"].texture);
+        boy.anchor.set(0.5); //设定中心锚点为中间
+        boy.x = app.screen.width / 2; //将位置设置为中间
+        boy.y = app.screen.height / 2;
+        // app.stage.addChild(tow);//将精灵贴图放进舞台中
+        let boyContainer = new Container();
+        boyContainer.addChild(boy);
+        app.stage.addChild(boyContainer);
+        boyContainer.position.set(0, 0);
+        app.ticker.add(() => {
+          // 每秒调用该方法60次(60帧动画)
+          boy.rotation += 0.06;
+        });
       }
-    );
-    document.body.appendChild(app.view);
-    //导入资源
-    loader.add('bed_boy',require("../imgs/bed_boy.png"))
-    .add('bed',require("../imgs/bed.png"))
-    .load(setup)
-    //进度加载
-    loader.onProgress.add((target)=>{
-      document.getElementById("loading").innerText = parseInt(target.progress) + "%";
-    })
-    //加载完成
-    loader.onComplete.add((target)=>{
-      document.getElementById('loading').style.display = 'none';
-    })
-    console.log(resources)
-    function setup(){
-            let boy = new Sprite(resources['bed_boy'].texture);
-            boy.anchor.set(0.5);//设定中心锚点为中间
-            boy.x = app.screen.width/2;//将位置设置为中间
-            boy.y = app.screen.height/2;
-           // app.stage.addChild(tow);//将精灵贴图放进舞台中
-            let boyContainer = new Container();
-            boyContainer.addChild(boy);
-            app.stage.addChild(boyContainer);
-            boyContainer.position.set(0,0);
-            app.ticker.add(() => {
-              // 每秒调用该方法60次(60帧动画)
-              boy.rotation += 0.06;
-            })
-         }
-    }
+    },
   },
 };
 </script>
